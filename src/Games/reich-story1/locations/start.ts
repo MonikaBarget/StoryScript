7d3543f45edae7352ab10ced7136043c55e56404
MonikaBarget <MonikaBarget@users.noreply.github.com>
2026-02-15 15:15:30 +0100

    Locations renamed.

--- src/Games/reich-story1/locations/start.ts
+++ src/Games/reich-story1/locations/start.ts
@@ -1,12 +1,37 @@
-﻿import { Location } from '../types'
-import description from './Start.html?raw'
+﻿import { Location } from '../types';
+import description from './Start.html?raw';
+// Import the raw HTML for each destination
+import warAndDefence from './Location1.html?raw';
+import currenciesAndFraud from './Location2.html?raw';
+import povertyAndOrder from './Location3.html?raw';
+import imperialGovernance from './Location4.html?raw';
+import migrationAndEconomies from './Location5.html?raw';
 
 export function Start() {
     return Location({
         name: 'Start',
         description: description,
         destinations: [
-            
-        ]
+            {
+                name: 'War and imperial defence',
+                file: warAndDefence,
+            },
+            {
+                name: 'Currencies and monetary fraud',
+                file: currenciesAndFraud,
+            },
+            {
+                name: 'Poverty and social order',
+                file: povertyAndOrder,
+            },
+            {
+                name: 'Imperial governance and law',
+                file: imperialGovernance,
+            },
+            {
+                name: 'Migration and cross-border economies',
+                file: migrationAndEconomies,
+            },
+        ],
     });
 }
