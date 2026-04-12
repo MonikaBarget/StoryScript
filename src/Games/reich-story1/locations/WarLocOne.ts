d95b2815c6731a95d42b302346ea01294ee206cf
MonikaBarget <MonikaBarget@users.noreply.github.com>
2026-04-12 14:21:09 +0200

    All new locations for War added!

--- src/Games/reich-story1/locations/WarLocOne.ts
+++ src/Games/reich-story1/locations/WarLocOne.ts
@@ -6,7 +6,27 @@ export function WarLocOne() {
     return Location({
         name: 'War & Defence',
         description: description,
-        destinations: [WarLocTwo
+        destinations: [
+            {
+    name: 'Wars against the Ottoman Empire',
+    target: LocWarTwo,
+},
+{
+    name: 'Military conflicts with Louis XIV, King of France',
+    target: LocWarThree,
+},
+{
+    name: 'War of the Polish Succession',
+    target: LocWarFour,
+},
+{
+    name: '1757 War against Prussia',
+    target: LocWarFive,
+},
+{
+    name: 'French Revolutionary and Napoleonic Wars',
+    target: LocWarSix,
+}
         ]
     });
 }
