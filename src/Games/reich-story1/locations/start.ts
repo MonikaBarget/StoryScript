7b2a58b68d05a7724edcd07d4fd3ac5f9ff5faf9
MonikaBarget <MonikaBarget@users.noreply.github.com>
2026-05-05 15:01:54 +0200

    missing comma added

--- src/Games/reich-story1/locations/start.ts
+++ src/Games/reich-story1/locations/start.ts
@@ -25,7 +25,7 @@ export function Start() {
                     ]
                 }
             }
-        ]
+        ],
         destinations: [
             {
                 name: 'War & Defence',
