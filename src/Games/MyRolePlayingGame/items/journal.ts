23c90636834ec4033df4a50424424f083c5911cc
Monika Barget <38257338+MonikaBarget@users.noreply.github.com>
2026-05-06 16:13:33 +0200

    Delete src/Games/MyRolePlayingGame directory

--- src/Games/MyRolePlayingGame/items/journal.ts
+++
@@ -1,9 +0,0 @@
-﻿import { Item } from '../types';
-import { EquipmentType } from 'storyScript/Interfaces/storyScript'
-
-export function Journal() {
-    return Item({
-        name: 'Joe\'s journal',
-        equipmentType: EquipmentType.Miscellaneous,
-    });
-}
