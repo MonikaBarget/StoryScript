6279362bdd09789a0abb47adc13b75a0cd023840
Monika Barget <38257338+MonikaBarget@users.noreply.github.com>
2026-05-06 16:33:11 +0200

    Delete src/Games/MyInteractiveStory directory

--- src/Games/MyInteractiveStory/customTexts.ts
+++
@@ -1,9 +0,0 @@
-﻿import { IInterfaceTexts } from 'storyScript/Interfaces/storyScript';
-
-export function CustomTexts(): IInterfaceTexts {
-        return {
-        // Add your custom texts here.
-        gameName: 'My Interactive Story',
-        youAreHere: 'Chapter {0}'
-    };
-}
