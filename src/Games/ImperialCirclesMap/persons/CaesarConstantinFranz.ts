748fd558045adbcc9eae29a8f4dea8853b53bd64
Monika Barget <38257338+MonikaBarget@users.noreply.github.com>
2026-06-05 07:55:37 +0000

    sources added

--- src/Games/ImperialCirclesMap/persons/CaesarConstantinFranz.ts
+++ src/Games/ImperialCirclesMap/persons/CaesarConstantinFranz.ts
@@ -4,6 +4,7 @@ import description from './CaesarConstantinFranz.html?raw';
 export function CaesarConstantinFranz() {
     return Person({
         name: 'Caesar Constantin Franz',
+        source: '<p>Lemarié, François. Mémoires Véridiques de La Révolution de Liege, Commencée Le 18 Août 1789 [Jusqu’au 18 Août 1789]. [Liège] A Liege, chez Lemarié, dessous La Tour. M.DCC.LXXXIX, 1789.</p>',
         description: description,
         hitpoints: 10,
         canAttack: false,
