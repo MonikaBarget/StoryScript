ce4e27b6e36405db0f55b9f5f5a0ae68426eefda
Monika Barget <38257338+MonikaBarget@users.noreply.github.com>
2026-06-05 08:02:45 +0000

    testing IIIF embedding

--- src/Games/ImperialCirclesMap/persons/CaesarConstantinFranz.ts
+++ src/Games/ImperialCirclesMap/persons/CaesarConstantinFranz.ts
@@ -4,7 +4,7 @@ import description from './CaesarConstantinFranz.html?raw';
 export function CaesarConstantinFranz() {
     return Person({
         name: 'Caesar Constantin Franz',
-        source: '<p>Lemarié, François. Mémoires Véridiques de La Révolution de Liege, Commencée Le 18 Août 1789 [Jusqu’au 18 Août 1789]. [Liège] A Liege, chez Lemarié, dessous La Tour. M.DCC.LXXXIX, 1789.</p>',
+        source: '<iframe src="https://uv-v4.netlify.app/uv.html#?manifest=https%3A%2F%2Fapi.digitale-sammlungen.de%2Fiiif%2Fpresentation%2Fv2%2Fbsb10275447%2Fmanifest&c=0&m=0&cv=0&config=&locales=en-GB%3AEnglish+%28GB%29%2Ccy-GB%3ACymraeg%2Cfr-FR%3AFran%C3%A7ais+%28FR%29%2Cpl-PL%3APolski%2Csv-SE%3ASvenska&xywh=-1766%2C-259%2C4783%2C2791&r=0" width="640" height="480" allowfullscreen frameborder="0" title="Précis Historique De La Révolution"></iframe>',
         description: description,
         hitpoints: 10,
         canAttack: false,
