4002b6f3b7750ca249fe1110440d7a8804394040
Monika Barget <38257338+MonikaBarget@users.noreply.github.com>
2026-06-07 18:48:11 +0000

    iframe resized

--- src/Games/ImperialCirclesMapMB/persons/MariaTheresia.ts
+++ src/Games/ImperialCirclesMapMB/persons/MariaTheresia.ts
@@ -7,7 +7,7 @@ export function MariaTheresia() {
         description: description,
         source: {
             name: 'Zuschrifft an Ihro Röm. Kayserliche auch zu Hungarn und Böheim Königl. Majestät, von des Nieder-Rhein-Westphälischen Creyßes Ausschreibender Herren Fürsten und Directoren zu Cölln anweesenden Directorial-Räthen und Gesandten',
-            description: '<iframe src="https://uv-v4.netlify.app/uv.html#?manifest=https%3A%2F%2Fapi.digitale-sammlungen.de%2Fiiif%2Fpresentation%2Fv2%2Fbsb10326330%2Fmanifest&c=0&m=0&cv=0&config=&locales=en-GB%3AEnglish+%28GB%29%2Ccy-GB%3ACymraeg%2Cfr-FR%3AFran%C3%A7ais+%28FR%29%2Cpl-PL%3APolski%2Csv-SE%3ASvenska&xywh=-2717%2C-347%2C7906%2C4613&r=0" width="640" height="480" allowfullscreen frameborder="0" title="Zuschrifft an Ihro Röm. Kayserliche auch zu Hungarn und Böheim Königl. Majestät, von des Nieder-Rhein-Westphälischen Creyßes Ausschreibender Herren Fürsten und Directoren zu Cölln anweesenden Directorial-Räthen und Gesandten"></iframe>'
+            description: '<iframe src="https://uv-v4.netlify.app/uv.html#?manifest=https%3A%2F%2Fapi.digitale-sammlungen.de%2Fiiif%2Fpresentation%2Fv2%2Fbsb10326330%2Fmanifest&c=0&m=0&cv=0&config=&locales=en-GB%3AEnglish+%28GB%29%2Ccy-GB%3ACymraeg%2Cfr-FR%3AFran%C3%A7ais+%28FR%29%2Cpl-PL%3APolski%2Csv-SE%3ASvenska&xywh=-2717%2C-347%2C7906%2C4613&r=0" width="560" height="420" allowfullscreen frameborder="0" title="Zuschrifft an Ihro Röm. Kayserliche auch zu Hungarn und Böheim Königl. Majestät, von des Nieder-Rhein-Westphälischen Creyßes Ausschreibender Herren Fürsten und Directoren zu Cölln anweesenden Directorial-Räthen und Gesandten"></iframe>'
         },
         hitpoints: 10,
         canAttack: false,
--- src/Games/ImperialCirclesMapMB/persons/MariaTheresia2.ts
+++ src/Games/ImperialCirclesMapMB/persons/MariaTheresia2.ts
@@ -8,7 +8,7 @@ export function MariaTheresia2() {
         description: description,
         source: {
             name: 'Cartel Zwischen Ihro Königlichen Majestät zu Hungarn und Böheim [et]c. [et]c. Dann Hohen Herren Fürsten und Ständen des Löblichen Fränckischen Crayses wegen Ausliefferung derer Deserteurs und anderen Militarischen Missethäteren',
-            description: '<iframe src="https://uv-v4.netlify.app/uv.html#?manifest=https%3A%2F%2Fapi.digitale-sammlungen.de%2Fiiif%2Fpresentation%2Fv2%2Fbsb10490100%2Fmanifest&c=0&m=0&cv=0&config=&locales=en-GB%3AEnglish+%28GB%29%2Ccy-GB%3ACymraeg%2Cfr-FR%3AFran%C3%A7ais+%28FR%29%2Cpl-PL%3APolski%2Csv-SE%3ASvenska&xywh=-2831%2C-297%2C7976%2C4654&r=0" width="640" height="480" allowfullscreen frameborder="0" title="Cartel Zwischen Ihro Königlichen Majestät zu Hungarn und Böheim [et]c. [et]c. Dann Hohen Herren Fürsten und Ständen des Löblichen Fränckischen Crayses wegen Ausliefferung derer Deserteurs und anderen Militarischen Missethäteren"></iframe>'
+            description: '<iframe src="https://uv-v4.netlify.app/uv.html#?manifest=https%3A%2F%2Fapi.digitale-sammlungen.de%2Fiiif%2Fpresentation%2Fv2%2Fbsb10490100%2Fmanifest&c=0&m=0&cv=0&config=&locales=en-GB%3AEnglish+%28GB%29%2Ccy-GB%3ACymraeg%2Cfr-FR%3AFran%C3%A7ais+%28FR%29%2Cpl-PL%3APolski%2Csv-SE%3ASvenska&xywh=-2831%2C-297%2C7976%2C4654&r=0" width="560" height="420" allowfullscreen frameborder="0" title="Cartel Zwischen Ihro Königlichen Majestät zu Hungarn und Böheim [et]c. [et]c. Dann Hohen Herren Fürsten und Ständen des Löblichen Fränckischen Crayses wegen Ausliefferung derer Deserteurs und anderen Militarischen Missethäteren"></iframe>'
         },
         hitpoints: 10,
         canAttack: false,
