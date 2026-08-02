bb38ebc0f8bba767469634332bb22310bd4e30cd
Monika Barget <38257338+MonikaBarget@users.noreply.github.com>
2026-08-02 15:26:41 +0000

    all destinations for CurLoc added

--- src/Games/ImperialCirclesThematic/locations/CurLoc_2.ts
+++ src/Games/ImperialCirclesThematic/locations/CurLoc_2.ts
@@ -1,21 +1,31 @@
 import { Location } from '../types';
 import description from './CurLoc_2.html?raw';
-import { CurLoc_1 } from './CurLoc_0';
-import { CurLoc_2 } from './CurLoc_1';
+import { CurLoc_1 } from './CurLoc_1';
+import { CurLoc_3 } from './CurLoc_3';
+import { CurLoc_4 } from './CurLoc_4';
+import { CurLoc_5 } from './CurLoc_5';
 
-export function CurLoc_3() {
+export function CurLoc_2() {
     return Location({
-        name: 'Fraud and trust in money',
+        name: 'Harmful Currency Practices',
         description: description,
         destinations: [
             {
-                name: 'Currencies & Fraud overview',
+                name: 'Currency Regulation',
                 target: CurLoc_1,
             },
             {
-                name: 'Currency systems and exchange',
-                target: CurLoc_2,
+                name: 'Counterfeit Currency',
+                target: CurLoc_3,
             },
-        ],
+            {
+                name: 'Foreign Currency',
+                target: CurLoc_4,
+            },
+            {
+                name: 'Unifying the German Currency System',
+                target: CurLoc_5,
+            },
+        ]
     });
 }
--- src/Games/ImperialCirclesThematic/locations/CurLoc_5.ts
+++ src/Games/ImperialCirclesThematic/locations/CurLoc_5.ts
@@ -1,9 +1,31 @@
 import { Location } from '../types';
-import description from './CurLoc_6.html?raw';
+import description from './CurLoc_5.html?raw';
+import { CurLoc_1 } from './CurLoc_1';
+import { CurLoc_2 } from './CurLoc_2';
+import { CurLoc_3 } from './CurLoc_3';
+import { CurLoc_4 } from './CurLoc_4';
 
-export function CurLoc_6() {
+export function CurLoc_5() {
     return Location({
-        name: 'CurLoc 6',
+        name: 'Unifying the German Currency System',
         description: description,
+        destinations: [
+            {
+                name: 'Currency Regulation',
+                target: CurLoc_1,
+            },
+            {
+                name: 'Harmful Currency Practices',
+                target: CurLoc_2,
+            },
+            {
+                name: 'Counterfeit Currency',
+                target: CurLoc_3,
+            },
+            {
+                name: 'Foreign Currency',
+                target: CurLoc_4,
+            },
+        ]
     });
 }
