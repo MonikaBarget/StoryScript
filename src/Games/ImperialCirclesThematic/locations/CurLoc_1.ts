885eb1e2dfa1993b278f745ed0fdbd55338dcd95
Monika Barget <38257338+MonikaBarget@users.noreply.github.com>
2026-08-02 15:20:28 +0000

    new destinations for CurLoc_1

--- src/Games/ImperialCirclesThematic/locations/CurLoc_1.ts
+++ src/Games/ImperialCirclesThematic/locations/CurLoc_1.ts
@@ -1,23 +1,31 @@
 import { Location } from '../types';
 import description from './CurLoc_1.html?raw';
 import { CurLoc_2 } from './CurLoc_2';
-import { CurLoc_3 } from './CurLoc_2';
-import { CurLoc_4 } from './CurLoc_3';
-import { CurLoc_5 } from './CurLoc_4';
+import { CurLoc_3 } from './CurLoc_3';
+import { CurLoc_4 } from './CurLoc_4';
+import { CurLoc_5 } from './CurLoc_5';
 
-export function CurLoc_2() {
+export function CurLoc_1() {
     return Location({
-        name: 'Currency systems and exchange',
+        name: 'Currency Regulation',
         description: description,
         destinations: [
             {
-                name: 'Currencies & Fraud overview',
-                target: CurLoc_1,
+                name: 'Harmful Currency Practices',
+                target: CurLoc_2,
             },
             {
-                name: 'Fraud and trust in money',
+                name: 'Counterfeit Currency',
                 target: CurLoc_3,
             },
-        ],
+            {
+                name: 'Foreign Currency',
+                target: CurLoc_4,
+            },
+            {
+                name: 'Unifying the German Currency System',
+                target: CurLoc_5,
+            },
+        ]
     });
 }
